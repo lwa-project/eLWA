4cd618c1346e013f983cc0b036164ce641d8060a
Jayce Dowell <jayce.dowell@gmail.com>
2017-04-14 19:00:07 +0000

    Initial work on adding support for VLA data recorded into 'GUPPI raw' format.

---
+++ guppi.py
@@ -0,0 +1,436 @@
+# -*- coding: utf-8 -*-
+"""
+Module for working with GUPPI raw data from the VLA
+
+$Rev$
+$LastChangedBy$
+$LastChangedDate$
+"""
+
+import copy
+import numpy
+
+from lsl import astro
+from lsl.reader.errors import syncError, eofError
+
+
+__version__ = '0.1'
+__revision__ = '$Rev$'
+__all__ = ['FrameHeader', 'FrameData', 'Frame', 'readGUPPIHeader', 'readFrame', 
+		 'getFrameSize', 'getThreadCount', 'getFramesPerSecond', 'getSampleRate', 
+		 'getCentralFreq', '__version__', '__revision__', '__all__']
+
+
+class FrameHeader(object):
+	"""
+	Class that stores the information found in the header of a VDIF 
+	frame.  Most fields in the VDIF version 1.1.1 header are stored.
+	"""
+	
+	def __init__(self, imjd=0, smjd=0, fmjd=0.0, offset=0.0, threadID=0, stationID=0, sampleRate=0.0, centralFreq=0.0):
+		self.imjd = imjd
+		self.smjd = smjd
+		self.fmjd = fmjd
+		self.offset = offset
+		
+		self.threadID = threadID
+		self.stationID = stationID
+		
+		self.sampleRate = sampleRate
+		self.centralFreq = centralFreq
+		
+	def getTime(self):
+		"""
+		Function to convert the time tag to seconds since the UNIX epoch.
+		"""
+		
+		mjd = self.imjd + (self.smjd + self.fmjd) / 86400.0
+		seconds = astro.utcjd_to_unix(mjd + astro.MJD_OFFSET)
+		seconds += self.offset / float(self.sampleRate)
+		
+		return seconds
+		
+	def parseID(self):
+		"""
+		Return a two-element tuple of the station ID and thread ID.
+		
+		.. note::
+			The station ID is always returned as numeric.
+		"""
+		return (self.stationID, self.threadID)
+		
+	def getSampleRate(self):
+		"""
+		Return the sample rate of the data in samples/second.
+		"""
+		
+		return self.sampleRate*1.0
+		
+	def getCentralFreq(self):
+		"""
+		Function to get the central frequency of the VDIF data in Hz.
+		"""
+		
+		return self.centralFreq*1.0
+
+
+class FrameData(object):
+	"""
+	Class that stores the information found in the data section of a VDIF
+	frame.
+	
+	.. note::
+		Unlike the other readers in the :mod:`lsl.reader` module the
+		data are stored as numpy.float32 values.
+	"""
+	
+	def __init__(self, data=None):
+		self.data = data
+
+
+class Frame(object):
+	"""
+	Class that stores the information contained within a single VDIF 
+	frame.  It's properties are FrameHeader and FrameData objects.
+	"""
+
+	def __init__(self, header=None, data=None):
+		if header is None:
+			self.header = FrameHeader()
+		else:
+			self.header = header
+			
+		if data is None:
+			self.data = FrameData()
+		else:
+			self.data = data
+			
+		self.valid = True
+
+	def parseID(self):
+		"""
+		Convenience wrapper for the Frame.FrameHeader.parseID 
+		function.
+		"""
+		
+		return self.header.parseID()
+		
+	def parseExtendedUserData(self):
+		"""
+		Convenience wrapper for the Frame.FrameHeader.parseExtendedUserData
+		function.
+		"""
+		
+		return self.header.parseExtendedUserData()
+		
+	def getTime(self):
+		"""
+		Convenience wrapper for the Frame.FrameHeader.getTime function.
+		"""
+		
+		return self.header.getTime()
+		
+	def getSampleRate(self):
+		"""
+		Convenience wrapper for the Frame.FrameHeader.getSampleRate function.
+		"""
+		
+		return self.header.getSampleRate()
+		
+	def getCentralFreq(self):
+		"""
+		Convenience wrapper for the Frame.FrameHeader.getCentralFreq function.
+		"""
+		
+		return self.header.getCentralFreq()
+		
+	def __add__(self, y):
+		"""
+		Add the data sections of two frames together or add a number 
+		to every element in the data section.
+		"""
+	
+		newFrame = copy.deepcopy(self)
+		newFrame += y	
+		return newFrame
+			
+	def __iadd__(self, y):
+		"""
+		In-place add the data sections of two frames together or add 
+		a number to every element in the data section.
+		"""
+		
+		try:
+			self.data.data += y.data.data
+		except AttributeError:
+			self.data.data += y
+		return self
+		
+	def __mul__(self, y):
+		"""
+		Multiple the data sections of two frames together or multiply 
+		a number to every element in the data section.
+		"""
+		
+		newFrame = copy.deepcopy(self)
+		newFrame *= y
+		return newFrame
+		
+	def __imul__(self, y):
+		"""
+		In-place multiple the data sections of two frames together or 
+		multiply a number to every element in the data section.
+		"""
+		
+		try:
+			self.data.data *= y.data.data
+		except AttributeError:
+			self.data.data *= y
+		return self
+
+
+def _filenameToAntenna(filename, vdifID=False):
+	try:
+		ant = filename.split('BD-', 1)[1]
+		ant = ant.split('.', 1)[0]
+		ant = int(ant, 10)
+	except (ValueError, IndexError):
+		return 0
+	
+	lookup = {0: 1, 
+			1: 3, 
+			2: 5, 
+			3: 9, 
+			4: 10, 
+			5: 11, 
+			6: 12, 
+			7: 13, 
+			8: 14, 
+			9: 18, 
+			10: 19, 
+			11: 23, 
+			12: 27}
+			
+	ant = ant[lookup]
+	if vdifID:
+		ant += 12300
+		
+	return ant
+
+
+_param_cache = {}
+
+
+def readGUPPIHeader(filehandle):
+	"""
+	Read in a GUPPI header at the start of a VDIF file from the VLA.  The 
+	contents of the header are returned as a dictionary.
+	"""
+	
+	global _param_cache
+	
+	# Read in the GUPPI header
+	header = {}
+	while True:
+		line = filehandle.read(80)
+		if line[:3] == 'END':
+			break
+		elif line[:8] == 'CONTINUE':
+			junk, value2 = line.split(None, 1)
+			value = "%s%s" % (value[:-1], value2[:-1])
+		else:
+			name, value = line.split('=', 1)
+			name = name.strip()
+		try:
+			value = int(value, 10)
+		except:
+			try:
+				value = float(value)
+			except:
+				value = value.strip().replace("'", '')
+		header[name.strip()] = value
+	header['OBSBW'] *= 1e6
+	header['OBSFREQ'] *= 1e6
+	
+	_param_cache[filehandle] = header
+	_param_cache[filehandle]['ANTENNA']   = _filenameToAntenna(filehandle.name, vdifID=True)
+	_param_cache[filehandle]['DATASTART'] = filehandle.tell()
+	_param_cache[filehandle]['LAST']      = filehandle.tell()
+	
+	return header
+
+
+def readFrame(filehandle, Verbose=False):
+	try:
+		_param_cache[filehandle]
+	except KeyError:
+		print "MISS"
+		
+		mark = filehandle.tell()
+		filehandle.seek(0)
+		readGUPPIHeader(filehandle)
+		filehandle.seek(mark)
+		
+	last      = _param_cache[filehandle]['LAST']
+	ant       = _param_cache[filehandle]['ANTENNA']
+	imjd      = _param_cache[filehandle]['STT_IMJD']
+	smjd      = _param_cache[filehandle]['STT_SMJD']
+	fmjd      = _param_cache[filehandle]['STT_OFFS']
+	ref       = _param_cache[filehandle]['DATASTART']
+	blocksize = _param_cache[filehandle]['BLOCSIZE']
+	pktsize   = _param_cache[filehandle]['PKTSIZE']
+	npkt      = _param_cache[filehandle]['NPKT']
+	nbits     = _param_cache[filehandle]['NBITS']
+	npol      = 2 if _param_cache[filehandle]['PKTFMT'].rstrip() == 'VDIF' else 1
+	srate     = _param_cache[filehandle]['OBSBW']
+	srate    *= 2 if _param_cache[filehandle]['PKTFMT'].rstrip() == 'VDIF' else 1
+	cfreq     = _param_cache[filehandle]['OBSFREQ']
+	
+	mark = filehandle.tell()
+	if 'SPARE' in _param_cache[filehandle].keys() and mark == last:
+		frame = _param_cache[filehandle]['SPARE']
+		del _param_cache[filehandle]['SPARE']
+		
+	else:
+		try:
+			raw = numpy.fromfile(filehandle, dtype=numpy.uint8, count=blocksize)
+			_param_cache[filehandle]['LAST'] = filehandle.tell()
+		except IOError:
+			raise eofError
+			
+		if nbits == 4:
+			data = numpy.zeros((2*raw.size), dtype=numpy.uint8)
+			data[1::2] = (raw & 0xF0) >> 4
+			data[0::2] = (raw & 0x0F)
+			data = data.astype(numpy.int8) - 8
+			data = data.reshape((-1,npol,2))
+			data = numpy.transpose(data, [1, 0, 2])
+		elif nbits == 8:
+			data = raw.astype(numpy.int8)
+		data = data.reshape((npol,-1))
+		
+		offset = mark - ref
+		offset /= blocksize
+		offset *= data.shape[1]
+		
+		dataX = data[0,:]
+		fhdr = FrameHeader(imjd=imjd, smjd=smjd, fmjd=fmjd, offset=offset, 
+						threadID=0, stationID=ant, sampleRate=srate, centralFreq=cfreq)
+		fdat = FrameData(data=dataX)
+		frame = Frame(header=fhdr, data=fdat)
+		
+		if npol == 2:
+			dataY = data[1,:]
+			fhdr = FrameHeader(imjd=imjd, smjd=smjd, fmjd=fmjd, offset=offset, 
+							threadID=1, stationID=ant, sampleRate=srate, centralFreq=cfreq)
+			fdat = FrameData(data=dataY)
+			_param_cache[filehandle]['SPARE'] = Frame(header=fhdr, data=fdat)
+			
+	return frame
+
+
+def getFrameSize(filehandle, nFrames=None):
+	"""
+	Find out what the frame size is in bytes from a single observation.
+	"""
+	
+	try:
+		_param_cache[filehandle]
+	except KeyError:
+		mark = filehandle.tell()
+		filehandle.seek(0)
+		readGUPPIHeader(filehandle)
+		filehandle.seek(mark)
+		
+	frameSize = _param_cache[filehandle]['BLOCSIZE']
+	
+	return frameSize
+
+
+def getThreadCount(filehandle):
+	"""
+	Find out how many thrads are present by examining the first 1024
+	records.  Return the number of threads found.
+	"""
+	
+	try:
+		_param_cache[filehandle]
+	except KeyError:
+		mark = filehandle.tell()
+		filehandle.seek(0)
+		readGUPPIHeader(filehandle)
+		filehandle.seek(mark)
+		
+	nPol = 2 if _param_cache[filehandle]['PKTFMT'].rstrip() == 'VDIF' else 1
+	
+	# Return the number of threads found
+	return nPol
+
+
+def getFramesPerSecond(filehandle):
+	"""
+	Find out the number of frames per second in a file by watching how the 
+	headers change.  Returns the number of frames in a second.
+	"""
+	
+	# Save the current position in the file so we can return to that point
+	fhStart = filehandle.tell()
+	
+	# Get the frame size
+	FrameSize = getFrameSize(filehandle)
+	
+	# Get the number of threads
+	nThreads = getThreadCount(filehandle)
+	
+	# Read in some frames
+	frames = []
+	for i in xrange(nThreads):
+		frames.append( readFrame(filehandle) )
+		
+	# Return to the place in the file where we started
+	filehandle.seek(fhStart)
+	
+	# Get the number of frames per second
+	nFramesSecond = int(frames[0].data.data.size / getSampleRate(filehandle))
+	
+	return nFramesSecond
+
+
+def getSampleRate(filehandle):
+	"""
+	Find and return the sample rate in Hz by looking at how many frames 
+	there are per second and how many samples there are in a frame.
+	"""
+	
+	try:
+		_param_cache[filehandle]
+	except KeyError:
+		mark = filehandle.tell()
+		filehandle.seek(0)
+		readGUPPIHeader(filehandle)
+		filehandle.seek(mark)
+		
+	sampleRate = _param_cache[filehandle]['OBSBW']
+	sampleRate *= 2 if _param_cache[filehandle]['PKTFMT'].rstrip() == 'VDIF' else 1
+	
+	# Return the sample rate
+	return sampleRate
+
+
+def getCentralFreq(filehandle):
+	"""
+	Find and return the central frequency in Hz.
+	"""
+	
+	try:
+		_param_cache[filehandle]
+	except KeyError:
+		mark = filehandle.tell()
+		filehandle.seek(0)
+		readGUPPIHeader(filehandle)
+		filehandle.seek(mark)
+		
+	centralFreq = _param_cache[filehandle]['OBSFREQ']
+	
+	# Return the observing frequency
+	return centralFreq
--- utils.py
+++ utils.py
@@ -8,6 +8,7 @@ $LastChangedBy$
 $LastChangedDate$
 """
 
+import os
 import re
 import ephem
 import numpy
@@ -21,10 +22,12 @@ from lsl.common.mcs import datetime2mjdmpm, delaytoMCSD, MCSDtodelay
 from lsl.common.metabundle import getCommandScript
 from lsl.misc.beamformer import calcDelay
 
+import guppi
+
 
 __version__ = '0.3'
 __revision__ = '$Rev$'
-__all__ = ['multiColumnPrint', 'parseTimeString', 'readCorrelatorConfiguration', 
+__all__ = ['EnhancedFixedBody', 'multiColumnPrint', 'parseTimeString', 'readCorrelatorConfiguration', 
 		 'getBetterTime', 'readGUPPIHeader', 'parseLWAMetaData', 'PolyCo', 'PolyCos', 
 		 '__version__', '__revision__', '__all__']
 
@@ -40,6 +43,129 @@ _srcs = ["ForA,f|J,03:22:41.70,-37:12:30.0,1",
 	    "3C196,f|J,08:13:36.06,+48:13:02.6,1",
 	    "3C286,f|J,13:31:08.29,+30:30:33.0,1",
 	    "3C295,f|J,14:11:20.47,+52:12:09.5,1", ]
+
+
+def _topo2baryMJD(src, observer):
+	"""
+	Given an EnhancedFixedBody instance, use TEMPO to determine an 
+	interpolating function that maps topocentric time to barycentric time.  
+	"""
+	
+	from residuals import read_residuals
+	
+	# Source position
+	ra = str(src.a_ra)
+	dec = str(src.a_dec)
+	
+	# Observatory for the observation - fixed on LWA1 until I can figure out how to do better
+	obs = 'LW'
+	
+	# Topocentric times to compute the barycentric times for
+	# NOTE:  This includes padding at the end for the fitting in TEMPO
+	if type(observer) is ephem.Observer:
+		topoMJD = observer.date + (astro.DJD_OFFSET - astro.MJD_OFFSET)
+	elif type(observer) is ephem.Date:
+		topoMJD = observer + (astro.DJD_OFFSET - astro.MJD_OFFSET)
+	else:
+		topoMJD = observer
+	topoMJD = topoMJD + numpy.linspace(0, 0.1, 11)
+	
+	# Write the TEMPO file for the conversion from topocentric to barycentric times
+	fh = open('bary.tmp', 'w')
+	fh.write("""C  Header Section
+  HEAD                   
+  PSR                 bary
+  NPRNT                  2
+  P0                   1.0 1
+  P1                   0.0
+  CLK            UTC(NIST)
+  PEPOCH           %19.13f
+  COORD              J2000
+  RA                    %s
+  DEC                   %s
+  DM                   0.0
+  EPHEM              DE200
+C  TOA Section (uses ITAO Format)
+C  First 8 columns must have + or -!
+  TOA\n""" % (topoMJD[0], ra, dec))
+	for t in topoMJD:
+		fh.write("topocen+ %19.13f  0.00     0.0000  0.000000  %s\n" % (t, obs))
+	fh.close()
+	
+	# Run TEMPO
+	status = os.system('tempo bary.tmp > barycorr.out')
+	if status != 0:
+		## This didn't work, skipping
+		print "WARNING: Could not run TEMPO, skipping conversion function calculation"
+		baryMJD = topoMJD[0]
+	else:
+		## Read in the barycentric times
+		resids = read_residuals()
+		baryMJD = resids.bary_TOA[0]
+		
+	# Cleanup
+	for filename in ('bary.tmp', 'bary.par', 'barycorr.out', 'resid2.tmp', 'tempo.lis', 'matrix.tmp'):
+		try:
+			os.unlink(filename)
+		except OSError:
+			pass
+			
+	# Return
+	return baryMJD
+
+
+class EnhancedFixedBody(ephem.FixedBody):
+	"""
+	Sub-class of ephem.FixedBody that allows for pulsar phase and frequency 
+	calculation.  This is done through a new '_polycos' attribute that is set
+	after initilization to a PolyCos instance.  On calling the 'compute' 
+	method, the 'phase' and 'frequency' attributes are set.
+	"""
+	
+	def __setattr__(self, name, value):
+		# Validate that the _polycos attribute is set to a PolyCo or PolyCos instance
+		if name == '_polycos':
+			if type(value) not in (PolyCo, PolyCos):
+				raise ValueError("Must set _polycos with a PolyCo or PolyCos instance")
+				
+		# Set the attribute if everything is ok
+		super(self.__class__, self).__setattr__(name, value)
+		
+	def __getattr__(self, name):
+		if name in ('phase', 'frequency', 'period'):
+			## Is this even valid?
+			if getattr(self, '_polycos', None) is None:
+				raise ValueError("Pulsar parameters cannot be determined for a non-pulsar body")
+				
+		# Get the attribute if every is ok
+		super(self.__class__, self).__getattr__(name)
+		
+	def compute(self, when=None, epoch=ephem.J2000):
+		# Basic validation
+		if when is None:
+			when = ephem.now()
+			
+		# Compute the basic source parameters via PyEphem
+		if type(when) is ephem.Observer:
+			super(self.__class__, self).compute(when)
+		else:
+			super(self.__class__, self).compute(when, epoch=epoch)
+			
+		# If there are polycos, compute the phase and frequency as well
+		if getattr(self, '_polycos', None) is not None:
+			## We can only really do this if we have an observer so
+			## that we can move to the barycenter
+			if type(when) is ephem.Observer:
+				mjd = _topo2baryMJD(self, when)
+				self.dm = self._polycos.getDM(mjd)
+				self.phase = self._polycos.getPhase(mjd)
+				self.frequency = self._polycos.getFrequency(mjd)
+				self.period = 1.0/self.frequency
+			else:
+				delattr(self, 'dm')
+				delattr(self, 'phase')
+				delattr(self, 'frequency')
+				delattr(self, 'period')
 
 
 def multiColumnPrint(items, sep=';  ', width=86):
@@ -176,11 +302,15 @@ def readCorrelatorConfiguration(filename):
 	
 	# Find the reference source
 	if 'ra' in sources[0].keys() and 'dec' in sources[0].keys():
-		refSource = ephem.FixedBody()
+		refSource = EnhancedFixedBody()
 		refSource.name = sources[0]['name']
 		refSource._ra = sources[0]['ra']
 		refSource._dec = sources[0]['dec']
 		refSource._epoch = ephem.J2000
+		try:
+			refSource._polycos = PolyCos(sources[0]['polyco'])
+		except KeyError:
+			pass
 	else:
 		srcs = [ephem.Sun(),]
 		for line in _srcs:
@@ -210,6 +340,8 @@ def readCorrelatorConfiguration(filename):
 	for block in blocks:
 		if block['type'] == 'vdif':
 			readers.append( vdif )
+		elif block['type'] == 'guppi':
+			readers.append( guppi )
 		elif block['type'] == 'drx':
 			readers.append( drx )
 		else:
@@ -260,9 +392,9 @@ def readCorrelatorConfiguration(filename):
 
 def getBetterTime(frame):
 	"""
-	Given a lsl.reader.vdif.Frame or lsl.reader.drx.Frame instance, return a 
-	more accurate time for the frame.  Unlike the Frame.getTime() functions, this
-	function returns a two-element tuple of:
+	Given a lsl.reader.vdif.Frame, guppi.Frame, or lsl.reader.drx.Frame 
+	instance, return a more accurate time for the frame.  Unlike the 
+	Frame.getTime() functions, this function returns a two-element tuple of:
 	 * integer seconds since the UNIX epoch and
 	 * fractional second
 	"""
@@ -279,10 +411,27 @@ def getBetterTime(frame):
 		frameRate = frame.header.sampleRate / nSamples
 		frac = frame.header.frameInSecond/frameRate
 		
-	else:
+	elif type(frame) == guppi.Frame:
+		mjd = frame.header.imjd + frame.header.smjd / 86400.0
+		sec = int(round(astro.utcjd_to_unix(mjd + astro.MJD_OFFSET)))
+		
+		offset = frame.header.offset
+		whol = offset / int(frame.getSampleRate())
+		frac = (offset - whol*int(frame.getSampleRate())) / float(frame.getSampleRate())
+		
+		sec += whol
+		frac += frame.header.fmjd
+		if frac >= 1.0:
+			sec += 1
+			frac -= 1.0
+			
+	elif type(frame) == drx.Frame:
 		tt = frame.data.timeTag - frame.header.timeOffset
 		sec = tt/196000000
 		frac = (tt - sec*196000000)/196e6
+		
+	else:
+		raise TypeError("Unknown frame type: %s" % type(frame).__name__)
 		
 	return [sec, frac]
 
@@ -465,6 +614,22 @@ class PolyCo(object):
 		self.validMinMJD = self.tMid - self.span/1440/2
 		self.validMaxMJD = self.tMid + self.span/1440/2
 		
+	def getDM(self, mjd):
+		"""
+		Given a MJD value, return the dispersion measure of the pulsar in 
+		pc cm^{-3}.
+		"""
+		
+		# Are we ready to go?
+		if getattr(self, 'tMid', None) is None:
+			raise RuntimeError("Need to populated from a .polyco file before using")
+			
+		# Is the MJD valid for this polyco?
+		if mjd < self.validMinMJD or mjd > self.validMaxMJD:
+			raise ValueError("PolyCo is only valid for MJD %.6f to %.6f" % (self.validMinMJD, self.validMaxMJD))
+			
+		return self.DM
+		
 	def getPhase(self, mjd):
 		"""
 		Given a MJD value, compute the phase of the pulsar.
@@ -488,7 +653,7 @@ class PolyCo(object):
 		
 	def getFrequency(self, mjd):
 		"""
-		Given a MJD value, compute the frequency of the pulsar.
+		Given a MJD value, compute the frequency of the pulsar in Hz.
 		"""
 		
 		# Are we ready to go?
@@ -548,6 +713,20 @@ class PolyCos(object):
 		self.tMids = numpy.array(m)
 		self.polyCos = p
 		
+	def getDM(self, mjd):
+		"""
+		Given a MJD value, return the dispersion measure of the pulsar in 
+		pc cm^{-3}.
+		"""
+		
+		# Are we ready to go?
+		if getattr(self, 'tMids', None) is None:
+			raise RuntimeError("Need to populated from a .polycos file before using")
+			
+		# Find the best polynomial to use
+		best = numpy.argmin( numpy.abs(self.tMids - mjd) )
+		return self.polyCos[best].getDM(mjd)
+		
 	def getPhase(self, mjd):
 		"""
 		Given a MJD value, compute the phase of the pulsar.
@@ -563,7 +742,7 @@ class PolyCos(object):
 		
 	def getFrequency(self, mjd):
 		"""
-		Given a MJD value, compute the frequency of the pulsar.
+		Given a MJD value, compute the frequency of the pulsar in Hz.
 		"""
 		
 		# Are we ready to go?
