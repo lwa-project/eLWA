d0383ec050fcfc22eeec66099f84a48b1bce802d
Jayce Dowell <jayce.dowell@gmail.com>
2018-03-07 17:01:20 +0000

    Added a couple of utilities to help run correlation jobs on the LWAUCF.

---
+++ launchJobs.py
@@ -0,0 +1,293 @@
+#!/usr/bin/env python
+
+"""
+Run a collection of correlation jobs on the LWAUCF
+
+$Rev$
+$LastChangedBy$
+$LastChangedDate$
+"""
+
+import os
+import re
+import sys
+import time
+import shlex
+import getopt
+import tempfile
+import threading
+import subprocess
+
+
+def usage(exitCode=None):
+	print """launchJobs.py - Given a collection of superCorrelator.py configuration files, 
+process the runs and aggregate the results.
+
+Usage:
+launchJobs.py [OPTIONS] config [config [...]]
+
+Options:
+-h, --help                Display this help information
+-p, --processes-per-node  Number of processes to run per node (Default = 1)
+-n, --nodes               Comma seperated lists of nodes to use 
+                          (Default = current)
+-o, --options             Correlator options to use
+                          (Default = -l 128 -t 1 -j)
+-r, --results-dir         Directory to put the results in 
+                          (Default = ./results')
+"""
+	
+	if exitCode is not None:
+		sys.exit(exitCode)
+	else:
+		return True
+
+
+def parseConfig(args):
+	config = {}
+	# Command line flags - default values
+	config['processes'] = 1
+	config['nodes'] = ['localhost',]
+	config['options'] = '-l 128 -t 1 -j'
+	config['results'] = './results'
+	config['args'] = []
+	
+	# Read in and process the command line flags
+	try:
+		opts, args = getopt.getopt(args, "hp:n:o:r:", ["help", "processes-per-node=", "nodes=", "options=", "results-dir="])
+	except getopt.GetoptError, err:
+		# Print help information and exit:
+		print str(err) # will print something like "option -a not recognized"
+		usage(exitCode=2)
+		
+	# Work through opts
+	for opt, value in opts:
+		if opt in ('-h', '--help'):
+			usage(exitCode=0)
+		elif opt in ('-p', '--processes-per-node'):
+			config['processes'] = int(value, 10)
+		elif opt in ('-n', '--nodes'):
+			config['nodes'] = [v.strip().rstrip() for v in value.split(',')]
+		elif opt in ('-o', '--options'):
+			config['options'] = value
+		elif opt in ('-r', '--results-dir'):
+			config['results'] = value
+		else:
+			assert False
+			
+	# Add in arguments
+	config['args'] = args
+	
+	# Validate
+	if config['processes'] <= 0:
+		raise RuntimeError('Invalid number of processes per node')
+	if os.path.exists(config['results']):
+		if not os.path.isdir(config['results']):
+			raise RuntimeError('%s is not a directory' % config['results'])
+	else:
+		print "Warning: %s does not exist, creating" % config['results']
+		os.mkdir(config['results'])
+		
+	# Return configuration
+	return config
+
+
+def run_command(cmd, node=None, cwd=None, quiet=False):
+	if node is None:
+		if type(cmd) is list:
+			pcmd = cmd
+		else:
+			pcmd = shlex.split(cmd)
+	elif cwd is None:
+		pcmd = ['ssh', '-t', '-t', node, 'bash -c "%s"' % cmd]
+	else:
+		pcmd = ['ssh', '-t', '-t', node, 'bash -c "cd %s && %s"' % (cwd, cmd)]
+		
+	DEVNULL = None
+	if quiet:
+		DEVNULL = open(os.devnull, 'wb')
+	p = subprocess.Popen(pcmd, stdout=DEVNULL, stderr=DEVNULL)
+	status = p.wait()
+	if quiet:
+		DEVNULL.close()
+		
+	return status
+
+
+def job(node, configfile, options='-l 128 -t 1 -j', softwareDir=None, resultsDir=None):
+	code = 0
+	
+	# Create a temporary directory to use
+	cwd = tempfile.mkdtemp(prefix='correlator-')
+	os.rmdir(cwd)
+	code += run_command('mkdir %s' % cwd, node=node, quiet=True)
+	if code != 0:
+		print "WARNING: failed to create directory on %s - %s" % (node, os.path.basename(configfile))
+		return False
+		
+	# Copy the software over
+	if softwareDir is None:
+		softwareDir = os.path.dirname(__file__)
+	for filename in ['buffer.py', 'guppi.py', 'jones.py', 'superCorrelator.py', 'utils.py', 'jit', configfile]:
+		filename = os.path.join(softwareDir, filename)
+		code += run_command('rsync -e ssh -avH %s %s:%s/' % (filename, node, cwd), quiet=True)
+	if code != 0:
+		print "WARNING: failed to sync software on %s - %s" % (node, os.path.basename(configfile))
+		return False
+		
+	# Run the correlator
+	configfile = os.path.basename(configfile)
+	outname, count = os.path.splitext(configfile)
+	try:
+		count = int(re.sub(r'\D*', '', count), 10)
+		outname = "%s%03i" % (outname, count)
+	except ValueError:
+		pass
+	if options.find('-w 1') != -1 or options.find('-w1') != -1:
+		outname += 'L'
+	elif options.find('-w 2') != -1 or options.find('-w2') != -1:
+		outname += 'H'
+	logfile = outname+".log"
+	#code += run_command('touch %s && touch %s.npz' % (logfile, outname), node=node, cwd=cwd)
+	code += run_command('./superCorrelator.py %s -g %s %s > %s 2>&1' % (options, outname, configfile, logfile), node=node, cwd=cwd)
+	if code != 0:
+		print "WARNING: failed to run correlator on %s - %s" % (node, os.path.basename(configfile))
+		return False
+		
+	# Gather the results
+	if resultsDir is None:
+		resultsDir = os.path.dirname(__file__)
+	code += run_command('rsync -e ssh -avH %s:%s/*.npz %s' % (node, cwd, resultsDir), quiet=True)
+	code += run_command('rsync -e ssh -avH %s:%s/*.log %s' % (node, cwd, resultsDir), quiet=True)
+	if code != 0:
+		print "WARNING: failed to sync results on %s - %s" % (node, os.path.basename(configfile))
+		return False
+		
+	# Cleanup
+	code += run_command('rm -rf %s' % cwd, node=node, quiet=True)
+	if code != 0:
+		print "WARNING: failed to remove directory on %s - %s" % (node, os.path.basename(configfile))
+		return False
+		
+	return True
+
+
+def any_active(threads, node=None):
+	active = False
+	for slot in threads.keys():
+		if node is not None and slot.split('-')[0] != node:
+			continue
+		if threads[slot] is not None:
+			active = True
+			break
+	return active
+
+
+def get_done_slot(threads):
+	done = None
+	for slot in threads.keys():
+		if threads[slot] is None:
+			continue
+		if not threads[slot].is_alive():
+			done = slot
+			break
+	return done
+
+
+def get_idle_slot(threads):
+	idle = None
+	for slot in threads.keys():
+		if threads[slot] is None:
+			idle = slot
+			break
+	return idle
+
+
+def create_lock_file(node):
+	if node[:6] == 'lwaucf':
+		fh = open('/home/jdowell/correlator%s' % node[6:], 'w')
+		fh.close()
+	return True
+
+
+def remove_lock_file(node):
+	if node[:6] == 'lwaucf':
+		try:
+			os.unlink('/home/jdowell/correlator%s' % node[6:])
+		except OSError:
+			pass
+	return True
+
+
+def main(args):
+	# Parse the command line
+	config = parseConfig(args)
+	
+	# Setup
+	## Sort
+	configfiles = config['args']
+	configfiles.sort(key=lambda x:[int(v) if v.isdigit() else v for v in re.findall(r'[^0-9]|[0-9]+', x)])
+	## Threads - processes by nodes so that small jobs are spread across jobs
+	threads = {}
+	for p in xrange(config['processes']):
+		for node in config['nodes']:
+			threads['%s-%02i' % (node, p)] = None
+			
+	# Start
+	for slot in sorted(threads.keys()):
+		node, _ = slot.split('-', 1)
+		
+		try:
+			configfile = configfiles.pop(0)
+			threads[slot] = threading.Thread(name=configfile, target=job, args=(node, configfile,), kwargs={'options':config['options'], 'resultsDir':config['results']})
+			threads[slot].daemon = True
+			threads[slot].start()
+			create_lock_file(node)
+			print "%s - %s started" % (slot, threads[slot].name)
+			time.sleep(2)
+		except IndexError:
+			continue
+			
+	# Wait and processes more
+	while any_active(threads):
+		## Check for completed jobs
+		done = get_done_slot(threads)
+		if done is not None:
+			print "%s - %s finished" % (done, threads[done].name)
+			threads[done] = None
+			
+		## Schedule new jobs
+		slot = get_idle_slot(threads)
+		if slot is not None:
+			node, _ = slot.split('-', 1)
+			
+			try:
+				configfile = configfiles.pop(0)
+				threads[slot] = threading.Thread(name=configfile, target=job, args=(node, configfile,), kwargs={'options':config['options'], 'resultsDir':config['results']})
+				threads[slot].daemon = True
+				threads[slot].start()
+				print "%s - %s started" % (slot, threads[slot].name)
+				time.sleep(2)
+			except IndexError:
+				continue
+				
+		## Lock file maintenance
+		for node in config['nodes']:
+			exited = any_active(threads, node=node)
+			if exited is None:
+				remove_lock_file(node)
+				
+		## Rest
+		time.sleep(10)
+		
+	# Teardown
+	for node in config['nodes']:
+		## Lock file cleanup
+		remove_lock_file(node)
+		
+	print "Done"
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
+	
---
+++ monitorJobs.py
@@ -0,0 +1,197 @@
+#!/usr/bin/env python
+
+"""
+Monitor jobs launch on the LWAUCF by launchJobs.py.
+
+$Rev$
+$LastChangedBy$
+$LastChangedDate$
+"""
+
+import os
+import re
+import sys
+import time
+import shlex
+import subprocess
+from datetime import datetime
+
+
+def run_command(cmd, node=None, cwd=None, quiet=False):
+	if node is None:
+		if type(cmd) is list:
+			pcmd = cmd
+		else:
+			pcmd = shlex.split(cmd)
+	elif cwd is None:
+		pcmd = ['ssh', '-t', '-t', node, 'bash -c "%s"' % cmd]
+	else:
+		pcmd = ['ssh', '-t', '-t', node, 'bash -c "cd %s && %s"' % (cwd, cmd)]
+		
+	outdev = subprocess.PIPE
+	if quiet:
+		outdev = open(os.devnull, 'wb')
+	p = subprocess.Popen(pcmd, stdout=outdev, stderr=outdev)
+	stdout, stderr = p.communicate()
+	status = p.returncode
+	if quiet:
+		outdev.close()
+		
+	return status, stdout, stderr
+
+
+def get_directories(node):
+	status, dirnames, errors = run_command('ls -d /tmp/correlator-*', node=node)
+	if status != 0:
+		dirnames = []
+	else:
+		dirnames = dirnames.split('\n')[:-1]
+		dirnames = [dirname.strip().rstrip() for dirname in dirnames]
+	return dirnames
+
+
+def get_processes(node):
+	status, processes, errors = run_command('ps aux | grep superCorrelator | grep bash | grep -v grep | grep -v ssh', node=node)
+	if status != 0:
+		processes = []
+	else:
+		processes = processes.split('\n')[:-1]
+		processes = [process.strip().rstrip() for process in processes]
+	return processes
+
+
+def get_directory_contents(node, dirname):
+	status, filenames, errors = run_command('ls -d %s/*' % dirname, node=node)
+	if status != 0:
+		filenames = []
+	else:
+		filenames = filenames.split('\n')[:-1]
+		filenames = [filename.strip().rstrip() for filename in filenames]
+	return filenames
+
+
+def get_logfile_speed(node, logname):
+	status, speedtime, error = run_command('grep -i -e average -e estimated %s | tail -n2' % logname, node=node)
+	if status != 0:
+		speed  = '---'
+		remain = '---'
+	else:
+		speedtime = speedtime.split('\n')[:-1]
+		speedtime = [entry.strip().rstrip() for entry in speedtime]
+		if len(speedtime) == 0:
+			speed  = '---'
+			remain = '---'
+		else:
+			speed  = '---'
+			remain = '---'
+			
+			for entry in speedtime:
+				if entry.find('verage') != -1:
+					_, speed = entry.rsplit('is', 1)
+				elif entry.find('estimated') != -1:
+					_, remain = entry.rsplit('is', 1)
+	return speed, remain
+
+
+def main(args):
+	try:
+		while True:
+			t0 = time.time()
+			
+			status = {}
+			for i in xrange(6):
+				node = 'lwaucf%i' % (i+1,)
+				
+				## Find out which directories exist
+				dirnames = get_directories(node)
+				if len(dirnames) == 0:
+					continue
+					
+				## Create an entry for this node since there seems to be
+				## something to report
+				status[node] = {'dirnames' :[], 
+				                'processes':[],
+				                'active'   :{},
+				                'progress' :{},
+				                'speed'    :{},
+				                'remaining':{}}
+				status[node]['dirnames'] = dirnames
+				
+				## Get running superCorrelator.py processes
+				status[node]['processes'] = get_processes(node)
+				
+				## For each process, get the configuration file being
+				## processes
+				for process in status[node]['processes']:
+					dirname, cmdname = process.split('&&', 1)
+					_, dirname = dirname.split('cd', 1)
+					dirname = dirname.strip().rstrip()
+					_, configname = cmdname.rsplit(None, 1)
+					status[node]['active'][dirname] = configname
+					
+				## For each directory, get the progress toward completion
+				## (the number of NPZ files) and the latest values from 
+				## the logfile for the average time per integration and the
+				## estimated time remaining
+				for dirname in status[node]['dirnames']:
+					### Filenames inside the directory
+					filenames = get_directory_contents(node, dirname)
+					
+					### Count the number of .npz files and find the .log
+					### file
+					nNPZ = 0
+					logname = None
+					for filename in filenames:
+						filename = filename.strip().rstrip()
+						_, ext = os.path.splitext(filename)
+						if ext == '.npz':
+							nNPZ += 1
+						elif ext == '.log':
+							logname = filename
+							
+					### Parse the logfile, if we have one
+					if logname is not None:
+						cspeed, cremain = get_logfile_speed(node, logname)
+					else:
+						cspeed, cremain = '---', '---'
+						
+					### Save
+					status[node]['progress'][dirname] = nNPZ
+					status[node]['speed'][dirname] = cspeed
+					status[node]['remaining'][dirname] = cremain
+			t1 = time.time()
+			
+			# Report
+			print "=== %s ===" % datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+			for node in status.keys():
+				entry = status[node]
+				
+				print "%s:" % node
+				print "  Directories: %i" % len(entry['dirnames'])
+				for dirname in entry['dirnames']:
+					nFiles = entry['progress'][dirname]
+					isActive = 'active' if dirname in entry['active'] else 'stale'
+					print "    %s -> %i .npz files, %s" % (dirname, nFiles, isActive)
+				print "  Processes: %i" % len(entry['processes'])
+				for dirname in entry['active']:
+					configfile = entry['active']
+					pid = 0
+					for process in entry['processes']:
+						if process.rsplit(None, 1)[1] == configfile:
+							pid = int(process.split(None)[1], 10)
+					speed = entry['speed'][dirname]
+					remaining = entry['remaining'][dirname]
+					print "    %s -> PID %i, %s per integration, %s remaining" % (dirname, pid, speed, remaining)
+			t2 = time.time()
+			print "query %.3f, report %.3f" % (t1-t0, t2-t1)
+			
+			# Sleep
+			time.sleep(60-(t2-t0))
+			
+	except KeyboardInterrupt:
+		pass
+
+
+if __name__ == "__main__":
+	main(sys.argv[1:])
+	
