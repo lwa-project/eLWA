39ac23499235d7eb4b23231e05956fc3c32193c0
jaycedowell <jayce.dowell@gmail.com>
2020-04-08 11:54:50 -0600

    Cleaned up the cleanup in simple_test.sh and did a little work on compare_results.py so that it actually compares something.

--- tests/simple_test.sh
+++ tests/simple_test.sh
@@ -23,19 +23,19 @@ if [[ ! -e ../get_vla_ant_pos.py ]]; then
 fi
 
 # Correlate eLWA data, build a FITS-IDI file, and flag it
-../createConfigFile.py -o elwa.config *
-../superCorrelator.py -t 1 -l 256 -j -g elwa elwa.config
-../buildIDI.py -t elwa elwa-*.npz
-../flagDelaySteps.py buildIDI_elwa.FITS_1
-../flagIDI.py buildIDI_elwa_flagged.FITS_1
+python ../createConfigFile.py -o elwa.config *
+python ../superCorrelator.py -t 1 -l 256 -j -g elwa elwa.config
+python ../buildIDI.py -t elwa elwa-*.npz
+python ../flagDelaySteps.py buildIDI_elwa.FITS_1
+python ../flagIDI.py buildIDI_elwa_flagged.FITS_1
 
 # Correlate LWA-only data, build a FITS-IDI file, and flag it
-../createConfigFile.py -o lwaonly.config 0* *.tgz
-../superCorrelator.py -t 1 -l 256 -j -w 1 -g lwaonlyL lwaonly.config
-../superCorrelator.py -t 1 -l 256 -j -w 2 -g lwaonlyH lwaonly.config
-../buildMultiBandIDI.py -t lwaonly lwaonly[LH]-*.npz
-../flagDelaySteps.py buildIDI_lwaonly.FITS_1
-../flagIDI.py buildIDI_lwaonly_flagged.FITS_1
+python ../createConfigFile.py -o lwaonly.config 0* *.tgz
+python ../superCorrelator.py -t 1 -l 256 -j -w 1 -g lwaonlyL lwaonly.config
+python ../superCorrelator.py -t 1 -l 256 -j -w 2 -g lwaonlyH lwaonly.config
+python ../buildMultiBandIDI.py -t lwaonly lwaonly[LH]-*.npz
+python ../flagDelaySteps.py buildIDI_lwaonly.FITS_1
+python ../flagIDI.py buildIDI_lwaonly_flagged.FITS_1
 
 # Download the reference data
 mkdir -p ref && cd ref
@@ -49,11 +49,11 @@ tar xzf eLWA_test_ref.tar.gz
 cd ..
 
 # Compare
-./compare_results.py buildIDI_elwa_flagged_flagged.FITS_1 ref/buildIDI_elwa_flagged_flagged.FITS_1
-./compare_results.py buildIDI_lwaonly_flagged_flagged.FITS_1 ref/buildIDI_lwaonly_flagged_flagged.FITS_1
+python ./compare_results.py buildIDI_elwa_flagged_flagged.FITS_1 ref/buildIDI_elwa_flagged_flagged.FITS_1
+python ./compare_results.py buildIDI_lwaonly_flagged_flagged.FITS_1 ref/buildIDI_lwaonly_flagged_flagged.FITS_1
 
-## Cleanup
-#rm -rf 0* *.vdif LT004_*.tgz
-#rm -rf *.config *.d
-#rm -rf *.npz *.FITS_*
-#rm -rf ref/*.FITS_*
+# Cleanup
+rm -rf 0* *.vdif LT004_*.tgz
+rm -rf *.config *.d
+rm -rf *.npz *.FITS_*
+rm -rf ref/*.FITS_*
