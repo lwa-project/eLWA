aef0d2475c17ccac31e822b525a26c2f5c0b7dab
jaycedowell <jayce.dowell@gmail.com>
2020-04-08 12:15:24 -0600

    Tried to fix some environment problems in Travis.  Disabled caching in .travis.yml.  Added some error handling to simple_test.sh.

--- tests/simple_test.sh
+++ tests/simple_test.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+# Setup
+## Exit when any command fails
+set -e
+## Keep track of the last executed command
+trap 'last_command=$current_command; current_command=$BASH_COMMAND' DEBUG
+## echo an error message before exiting
+trap 'echo "\"${last_command}\" command filed with exit code $?."' EXIT
+
 # Download the raw data
-if [[ -e eLWA_test_raw.tar.gz ]]; then
-    curlflags="-z eLWA_test_raw.tar.gz"
-else
-    curlflags=
-fi
-curl https://fornax.phys.unm.edu/lwa/data/eLWA_test_raw.tar.gz -o eLWA_test_raw.tar.gz ${curlflags}
+curl https://fornax.phys.unm.edu/lwa/data/eLWA_test_raw.tar.gz -o eLWA_test_raw.tar.gz
 tar xzf eLWA_test_raw.tar.gz
 
 # Write a stub get_vla_ant_pos.py to use if we need it
@@ -39,21 +42,10 @@ python ../flagIDI.py buildIDI_lwaonly_flagged.FITS_1
 
 # Download the reference data
 mkdir -p ref && cd ref
-if [[ -e eLWA_test_ref.tar.gz ]]; then
-    curlflags="-z eLWA_test_ref.tar.gz"
-else
-    curlflags=
-fi
-curl https://fornax.phys.unm.edu/lwa/data/eLWA_test_ref.tar.gz -o eLWA_test_ref.tar.gz ${curlflags}
+curl https://fornax.phys.unm.edu/lwa/data/eLWA_test_ref.tar.gz -o eLWA_test_ref.tar.gz
 tar xzf eLWA_test_ref.tar.gz
 cd ..
 
 # Compare
 python ./compare_results.py buildIDI_elwa_flagged_flagged.FITS_1 ref/buildIDI_elwa_flagged_flagged.FITS_1
 python ./compare_results.py buildIDI_lwaonly_flagged_flagged.FITS_1 ref/buildIDI_lwaonly_flagged_flagged.FITS_1
-
-# Cleanup
-rm -rf 0* *.vdif LT004_*.tgz
-rm -rf *.config *.d
-rm -rf *.npz *.FITS_*
-rm -rf ref/*.FITS_*
