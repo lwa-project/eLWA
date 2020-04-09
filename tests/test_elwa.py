0a18831477c0df37aa7080b27d0b2542d8592871
jaycedowell <jdowell@unm.edu>
2020-04-09 10:37:43 -0600

    Moved the test suite over to unittest so that we can integrate in script testing with pylint.

---
+++ tests/test_elwa.py
@@ -0,0 +1,186 @@
+# -*- coding: utf-8 -*-
+
+"""
+Unit tests for the a small eLWA correlation job.
+"""
+
+# Python3 compatibility
+from __future__ import print_function, division, absolute_import
+import sys
+if sys.version_info > (3,):
+    xrange = range
+    
+import unittest
+unittest.TestLoader.sortTestMethodsUsing = None
+
+import os
+import glob
+import numpy
+import pyfits
+import subprocess
+
+_RAW = 'eLWA_test_raw.tar.gz'
+_REF = 'eLWA_test_ref.tar.gz'
+
+
+class eLWA_tests(unittest.TestCase):
+    def setUp(self):
+        """Make sure we have the comparison files in place."""
+        
+        # get_vla_ant_pos.py
+        if not os.path.exists('../get_vla_ant_pos.py'):
+            with open('../get_vla_ant_pos.py', 'w') as fh:
+                fh.write("""class database(object):
+    def __init__(self, *args, **kwds):\
+        self._ready = True
+    def get_pad(self,ant,date):
+        return 'W40', None
+    def get_xyz(self,ant,date):
+        return (-6777.0613, -360.7018, -3550.9465)
+    def close(self):
+        return True""")
+            
+        # Raw data
+        if not os.path.exists(_RAW)):
+            subprocess.check_call(['curl',
+                                   'https://fornax.phys.unm.edu/lwa/data/%s' % _RAW,
+                                   '-o', _RAW])
+        subprocess.check_call(['tar', 'xzvf', _RAW])
+        
+        # Reference data
+        if not os.path.exists('ref')):
+            os.mkdir('ref'))
+            
+        if not os.path.exits('ref/%s' % _REF):
+            subprocess.check_call(['curl',
+                                   'https://fornax.phys.unm.edu/lwa/data/%s' % _RAW,
+                                   '-o', 'ref/%s' % _REF
+        subprocess.check_call(['tar', 'xzvf', '-C', 'ref', 'ref/%s' % _RAW])
+        
+        # Other variables
+        self._FILES = ['0*', '*.vdif', 'LT004_*.tgz']
+        self._BASENAME = 'elwa'
+        
+    def create(self):
+        """Build the correlator configuration file."""
+        
+        files = []
+        for regex in self._FILES:
+            files.extend(glob.glob(regex))
+            
+        cmd = ['python', '../createConfigFile.py', '-o', '%s.config' % self._BASNENAME]
+        cmd.extend(files)
+        status = subprocess.check_call(cmd)
+        self.assertEqual(status, 0)
+        
+    def correlate(self):
+        """Run the correlator on eLWA data."""
+        
+        cmd = ['python', '../superCorrelator.py', '-t', '1', '-l', '256', 
+               '-j', '-g', self._BASENAME, '%s.config' % self._BASENAME]
+        with open('%s-correlate.log' % self._BASENAME, 'w') as logfile:
+            status = subprocess.check_call(cmd, stdout=logfile)
+        self.assertEqual(status, 0)
+        
+    def build(self):
+        """Build a FITS-IDI file for the eLWA data."""
+        
+        files = glob.glob('%s-*.npz' % self._BASENAME)
+        cmd = ['python', '../buildIDI.py', '-t', self._BASENAME]
+        cmd.extend(files)
+        with open('%s-build.log' % self._BASENAME, 'w') as logfile:
+            status = subprocess.check_call(cmd, stdout=logfile)
+        self.assertEqual(status, 0)
+        
+    def flag_steps(self):
+        """Flag LWA delay steps in the FITS-IDI file."""
+        
+        cmd = ['python', '../flagDelaySteps.py', 'buildIDI_%s.FITS_1' % self._BASENAME]
+        with open('%s-flag-0.log' % self._BASENAME, 'w') as logfile:
+            status = subprocess.check_call(cmd, stdout=logfile)
+        self.assertEqual(status, 0)
+        
+    def flag_rfi(self):
+        """Flag interference in the FITS-IDI file."""
+        
+        cmd = ['python', '../flagIDI.py', 'buildIDI_%s_flagged.FITS_1' % self._BASENAME]
+        with open('%s-flag-1.log' % self._BASENAME, 'w') as logfile:
+            status = subprocess.check_call(cmd, stdout=logfile)
+        self.assertEqual(status, 0)
+        
+    def validate_headers(self):
+        """Validate the headers of the flagged FITS-IDI file against the reference."""
+        
+        hdulist1 = pyfits.open('buildIDI_%s_flagged_flagged.FITS_1' % self._BASENAME,
+                               mode='readonly')
+        hdulist2 = pyfits.open('./ref/buildIDI_%s_flagged_flagged.FITS_1' % self._BASENAME,
+                               mode='readonly')
+        
+        # Loop through the HDUs
+        for hdu1,hdu2 in zip(hdulist1, hdulist2):
+            ## Check the header values, modulo the old $Rev$ tag
+            for key in hdu1.header:
+                if key in ('DATE-MAP',):
+                    continue
+                h1 = re.sub(_revRE, '', str(hdu1.header[key]))
+                h2 = re.sub(_revRE, '', str(hdu2.header[key]))
+                self.assertEqual(h1, h2, "Mis-match on %s: '%s' != '%s'" % (key, h1, h2))
+                
+        hdulist1.close()
+        hdulist2.close()
+        
+    def validate_data(self):
+        """Validate the data in the flagged FITS-IDI file against the reference."""
+        
+        hdulist1 = pyfits.open('buildIDI_%s_flagged_flagged.FITS_1' % self._BASENAME,
+                               mode='readonly')
+        hdulist2 = pyfits.open('./ref/buildIDI_%s_flagged_flagged.FITS_1' % self._BASENAME,
+                               mode='readonly')
+        
+        # Loop through the HDUs
+        for hdu1,hdu2 in zip(hdulist1, hdulist2):
+            ## Skip over the PRIMARY header
+            if hdu1.name == 'PRIMARY':
+                continue
+                
+            r = 0
+            for row1,row2 in zip(hdu1.data, hdu2.data):
+                for f in range(len(row1)):
+                    try:
+                        same_value = numpy.allclose(row1[f], row2[f], atol=6e-7)
+                    except TypeError:
+                        same_value = numpy.array_equal(row1[f], row2[f])
+                    self.assertTrue(same_value, "row %i, field %i (%s) does not match" % (r, f, hdu1.data.columns[f]))
+                    
+        hdulist1.close()
+        hdulist2.close()
+        
+    def tearDown(self):
+        """Cleanup"""
+        
+        for regex in ('%s.config' % self._BASENAME,
+                      '%s-*.log' % self._BASENAME,
+                      '%s-*.npz' % self._BASENAME,
+                      'buildIDI_%s_*.FITS_*' % self._BASENAME,
+                      '*.d'):
+            files = glob.glob(regex)
+            for file in files:
+                try:
+                    os.unlink(file)
+                except OSError:
+                    pass
+
+
+class eLWA_test_suite(unittest.TestSuite):
+    """A unittest.TestSuite class which contains all of the eLWA correlation tests."""
+    
+    def __init__(self):
+        unittest.TestSuite.__init__(self)
+        
+        loader = unittest.TestLoader()
+        self.addTests(loader.loadTestsFromTestCase(eLWA_tests)) 
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
---
+++ tests/test_scripts.py
@@ -0,0 +1,146 @@
+# -*- coding: utf-8 -*-
+
+"""
+Unit tests for the various eLWA scripts.
+"""
+
+# Python3 compatibility
+from __future__ import print_function, division, absolute_import
+import sys
+if sys.version_info > (3,):
+    xrange = range
+    
+import unittest
+import glob
+import sys
+import imp
+import re
+import os
+
+currentDir = os.path.abspath(os.getcwd())
+if os.path.exists(os.path.join(currentDir, 'test_scripts.py')):
+    MODULE_BUILD = currentDir
+else:
+    MODULE_BUILD = None
+    
+run_scripts_tests = False
+try:
+    from pylint import epylint as lint
+    if MODULE_BUILD is not None:
+        run_scripts_tests = True
+except ImportError:
+    pass
+
+
+__version__  = "0.1"
+__author__   = "Jayce Dowell"
+
+
+_LINT_RE = re.compile('(?P<module>.*?)\:(?P<line>\d+)\: (error )?[\[\(](?P<type>.*?)[\]\)] (?P<info>.*)')
+
+
+_SAFE_TO_IGNORE = ["Module 'numpy",
+                   "Module 'ephem"]
+
+
+def _get_context(filename, line, before=0, after=0):
+    to_save = range(line-before, line+after+1)
+    context = []
+    with open(filename, 'r') as fh:
+        i = 0
+        for line in fh:
+            if i in to_save:
+                context.append(line)
+            i += 1
+    return context
+
+
+@unittest.skipUnless(run_scripts_tests, "requires the 'pylint' module")
+class scripts_tests(unittest.TestCase):
+    """A unittest.TestCase collection of unit tests for the commissioning scripts."""
+    
+    pass     
+
+
+def _test_generator(script):
+    """
+    Function to build a test method for each script that is provided.  
+    Returns a function that is suitable as a method inside a unittest.TestCase
+    class
+    """
+    
+    def test(self):
+        out, err = lint.py_run("%s -E" % script, return_std=True)
+        out_lines = out.read().split('\n')
+        err_lines = err.read().split('\n')
+        out.close()
+        err.close()
+        
+        for line in out_lines:
+            ignore = False
+            for phrase in _SAFE_TO_IGNORE:
+                if line.find(phrase) != -1:
+                    ignore = True
+                    break
+            if ignore:
+                continue
+                
+            mtch = _LINT_RE.match(line)
+            if mtch is not None:
+                line_no, type, info = mtch.group('line'), mtch.group('type'), mtch.group('info')
+                ignore = False
+                if line.find('before assignment') != -1:
+                    context = _get_context(script, int(line_no), before=20, after=20)
+                    loc = context[20-1]
+                    level = len(loc) - len(loc.lstrip()) - 4
+                    found_try = None
+                    for i in range(2, 20):
+                        if context[20-i][level:level+3] == 'try':
+                            found_try = i
+                            break
+                    found_except = None
+                    for i in range(0, 20):
+                        if context[20+i][level:level+6] == 'except':
+                            found_except = i
+                            break
+                    if found_try is not None and found_except is not None:
+                        if context[20+found_except].find('NameError') != -1:
+                            ignore = True
+                if ignore:
+                    continue
+                    
+                self.assertEqual(type, None, "%s:%s - %s" % (os.path.basename(script), line_no, info))
+    return test
+
+
+if run_scripts_tests:
+    _SCRIPTS = glob.glob(os.path.join(MODULE_BUILD, '..', '*.py'))
+    for depth in range(1, 3):
+        path = [MODULE_BUILD, '..']
+        path.extend(['*',]*depth)
+        path.append('*.py')
+        _SCRIPTS.extend(glob.glob(os.path.join(*path)))
+    _SCRIPTS = list(filter(lambda x: x.find('test_scripts.py') == -1, _SCRIPTS))
+    _SCRIPTS.sort()
+    for script in _SCRIPTS:
+        test = _test_generator(script)
+        name = 'test_%s' % os.path.splitext(os.path.basename(script))[0]
+        doc = """Static analysis of the '%s' script.""" % os.path.basename(script)
+        setattr(test, '__doc__', doc)
+        setattr(scripts_tests, name, test)
+
+
+class scripts_test_suite(unittest.TestSuite):
+    """A unittest.TestSuite class which contains all of the commissioning script
+    tests."""
+    
+    def __init__(self):
+        unittest.TestSuite.__init__(self)
+        
+        loader = unittest.TestLoader()
+        self.addTests(loader.loadTestsFromTestCase(scripts_tests))
+
+
+if __name__ == '__main__':
+    unittest.main()
+    
