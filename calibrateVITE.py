3d6048ee98a65d12085b52092c2fb5f41f108a6d
jaycedowell <jayce.dowell@gmail.com>
2021-01-22 17:14:37 -0700

    Typo.

--- calibrateVITE.py
+++ calibrateVITE.py
@@ -388,7 +388,7 @@ def main(args):
     gains_cl = {0: gains_cl_p0, 1: gains_cl_p1}
     gains_bp_p0, gains_bp_p1 = load_caltab_bp(args.caltab)
     gains_bp = {0: gains_bp_p0, 1: gains_bp_p1}
-    delays_sm_p0, dleays_sm_p1, gains_sm_p0, gains_sm_p1 = load_cal_cl(args.cal)
+    delays_sm_p0, delays_sm_p1, gains_sm_p0, gains_sm_p1 = load_cal_cl(args.cal)
     delays_sm = {0: delays_sm_p0, 1: delays_sm_p1}
     gains_sm = {0: gains_sm_p0, 1: gains_sm_p1}
     if args.apply_uvout:
