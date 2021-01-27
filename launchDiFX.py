1f7ca67f2e547059ede93cce56c12cb504e5bc19
jaycedowell <jayce.dowell@gmail.com>
2021-01-27 16:25:34 -0700

    Added launchDiFX.py from difx-vlite-fast.

---
+++ launchDiFX.py
@@ -0,0 +1,133 @@
+#!/usr/bin/env python3
+
+import os
+import sys
+import time
+import shutil
+import argparse
+import subprocess
+
+
+def main(args):
+    # Parse the command line arguments
+    inputfile = args.filename
+    machinesfile = inputfile.replace('.input', '.machines')
+    
+    # Check that DIFX_VERSION is set and that we have everything we need in
+    # our path
+    if os.getenv('DIFX_VERSION', None) is None:
+        print("WARNING: DIFX_VERSION is not set")
+    for cmd in ('mpirun', 'mpifxcorr', 'difxcalc', 'difx2fits'):
+        status = subprocess.check_call(['which', cmd],
+                                       stdout=subprocess.DEVNULL,
+                                       stderr=subprocess.DEVNULL)
+        if status != 0:
+            raise RuntimeError("Cannot find '%s' in the current $PATH" % cmd)
+            
+    # Look for the existance of the .calc and .threads file as well as the
+    # output.  If either of the first two are missing, we cannot proceede.
+    # If the output exists, we cannot proceed.
+    calcfile = None
+    corefile = None
+    outname = None
+    with open(inputfile, 'r') as fh:
+        for line in fh:
+            if line.find('CALC FILENAME:') != -1:
+                calcfile = line.split(':', 1)[1]
+                calcfile = calcfile.strip().rstrip()
+            elif line.find('CORE CONF FILENAME:') != -1:
+                corefile = line.split(':', 1)[1]
+                corefile = corefile.strip().rstrip()
+            elif line.find('OUTPUT FILENAME:') != -1:
+                outname = line.split(':', 1)[1]
+                outname = outname.strip().rstrip()
+    if not os.path.exists(calcfile):
+        raise RuntimeError("Cannot find calc file: '%s'" % calcfile)
+    if not os.path.exists(corefile):
+        raise RuntimeError("Cannot find core file: '%s'" % corefile)
+    if os.path.exists(outname):
+        if args.force:
+            if os.path.isdir(outname):
+                shutil.rmtree(outname)
+            else:
+                os.unlink(outname)
+        else:
+            if os.path.isdir(outname):
+                raise RuntimeError("Output directory already exists: '%s'" % outname)
+            else:
+                raise RuntimeError("Output file already exists: '%s'" % outname)
+                
+    # Look for the existance of the .im file.  If it doesn't exist, try to
+    # create it by running 'difxcalc'.
+    obscode = None
+    imfile = None
+    with open(calcfile, 'r') as fh:
+        for line in fh:
+            if line.find('OBSCODE:') != -1:
+                obscode = line.split(':', 1)[1]
+                obscode = obscode.strip().rstrip()
+            elif line.find('IM FILENAME:') != -1:
+                imfile = line.split(':', 1)[1]
+                imfile = imfile.strip().rstrip()
+    if not os.path.exists(imfile) or args.force:
+        try:
+            os.unlink(imfile)
+        except OSError:
+            pass
+        logname = os.path.splitext(os.path.basename(inputfile))[0]+'.difxcalc_log'
+        errname = logname.replace('_log', '_err')
+        with open(logname, 'wb') as lf:
+            with open(errname, 'wb') as ef:
+                subprocess.check_call(['difxcalc', calcfile],
+                                      stdout=lf, stderr=ef,
+                                      cwd=os.path.dirname(imfile))
+    if not os.path.exists(imfile):
+        raise RuntimeError("Cannot find im file: '%s'" % imfile)
+    for ext in ('.FITS', '.log', '_setup1.jobmatrix'):
+        _outname = f"{obscode}.0.bin0000.source0000{ext}"
+        if os.path.exists(_outname):
+            if args.force:
+                os.unlink(_outname)
+            else:
+                raise RuntimeError("Output file already exists: '%s'" % _outname)
+                
+    # Build up the 'mpifxcorr' command to run.
+    cmd = ['mpirun',]
+    if os.path.exists(machinesfile):
+        cmd.extend(['-hostfile', machinesfile])
+    cmd.extend(['mpifxcorr', inputfile])
+    
+    # Correlate!
+    logname = os.path.splitext(os.path.basename(inputfile))[0]+'.mpifxcorr_log'
+    errname = logname.replace('_log', '_err')
+    t0 = time.time()
+    with open(logname, 'wb') as lf:
+        with open(errname, 'wb') as ef:
+                subprocess.check_call(cmd, stdout=lf, stderr=ef)
+    print("Finished correlation in %.3f s" % ((time.time()-t0),))
+    
+    # Build the 'difx2fits' command to run.
+    cmd = ['difx2fits', os.path.splitext(outname)[0]]
+    
+    # Convert!
+    logname = os.path.splitext(os.path.basename(inputfile))[0]+'.difx2fits_log'
+    errname = logname.replace('_log', '_err')
+    t0 = time.time()
+    with open(logname, 'wb') as lf:
+        with open(errname, 'wb') as ef:
+            subprocess.check_call(cmd, stdout=lf, stderr=ef)
+    print("Finished data conversion in %.3f s" % ((time.time()-t0),))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(
+        description="run DiFX on the specified configuration file",
+        formatter_class=argparse.ArgumentDefaultsHelpFormatter
+        )
+    parser.add_argument('filename', type=str, 
+                        help='DiFX .input configuration file')
+    parser.add_argument('-f', '--force', action='store_true',
+                        help='overwrite output files as needed')
+    args = parser.parse_args()
+    main(args)
+    
