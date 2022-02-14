b10223e9815b4c063c730c784c471de70138820f
jaycedowell <jaycedowell@gmail.com>
2022-02-14 15:13:53 -0700

    Cleanup plus fixes.

--- gpu/misc.py
+++ gpu/misc.py
@@ -13,16 +13,16 @@ from .cache import get_from_shape, get_from_ndarray, copy_using_cache
 
 
 __version__ = '0.1'
-__all__ = ['inplace_roll', 'pol_fill']
+__all__ = ['inplace_freq_roll', 'pol_fill']
 
 
-_INPLACE_ROLL = cp.RawKernel(r"""
+_INPLACE_FREQ_ROLL = cp.RawKernel(r"""
 extern "C" __global__
-void inplace_roll(float2* signals,
-                  int offset,
-                  int nStand,
-                  int nChan,
-                  int nWin) {
+void inplace_freq_roll(float2* signals,
+                       int offset,
+                       int nStand,
+                       int nChan,
+                       int nWin) {
   int s = blockIdx.x*blockDim.x + threadIdx.x;
   int w = blockIdx.y*blockDim.y + threadIdx.y;
   
@@ -31,17 +31,18 @@ void inplace_roll(float2* signals,
   } else {
     int c;
     float2 temp;
-    for(c=offset; c<nChan; c++) {
+    for(c=-offset; c<nChan; c++) {
       temp = *(signals + s*nChan*nWin + c*nWin + w);
-      *(signals + s*nChan*nWin + (c-offset)*nWin + w) = temp;
+      *(signals + s*nChan*nWin + (c+offset)*nWin + w) = temp;
     }
   }
 }
-""", 'inplace_roll')
+""", 'inplace_freq_roll')
 
 
-def inplace_roll(signals, offset, blockDim=(4,16)):
+def inplace_freq_roll(signals, offset, blockDim=(4,16)):
     nStand, nChan, nWin = signals.shape
+    assert(offset <= 0)
     
     signals = copy_using_cache(signals)
     if offset == 0:
@@ -51,9 +52,9 @@ def inplace_roll(signals, offset, blockDim=(4,16)):
     nab = int(np.ceil(nStand/nat))
     nwb = int(np.ceil(nWin/nwt))
     
-    _INPLACE_ROLL((nab,nwb), (nat,nwt),
-                  (signals, cp.int32(offset),
-                   cp.int32(nStand), cp.int32(nChan), cp.int32(nWin)))
+    _INPLACE_FREQ_ROLL((nab,nwb), (nat,nwt),
+                       (signals, cp.int32(offset),
+                        cp.int32(nStand), cp.int32(nChan), cp.int32(nWin)))
     
     return signals
 
@@ -80,23 +81,28 @@ void pol_fill(const float2 *signals,
     // Nothin'
   } else {
     int s;
-    for(s=0; s<nStand; s+=2) {
-      float2 tempA = *(signals + (s+0)*nChanAct*nWinAct + c*nWinAct + w);
-      float2 tempB = *(signals + (s+1)*nChanAct*nWinAct + c*nWinAct + w);
-      unsigned char validA = *(valid + (s+0)*nWinAct + w);
-      unsigned char validB = *(valid + (s+1)*nWinAct + w);
+    float2 tempA, tempB, tempT;
+    unsigned char validA, validB, validT;
+    for(s=0; s<nStand/2; s++) {
+      tempA = *(signals + (2*s+0)*nChanAct*nWinAct + c*nWinAct + w);
+      tempB = *(signals + (2*s+1)*nChanAct*nWinAct + c*nWinAct + w);
+      validA = *(valid + (2*s+0)*nWinAct + w);
+      validB = *(valid + (2*s+1)*nWinAct + w);
       
-      if( *(swap + s/2) ) {
-        *(outputX + (s/2+offset)*nChan*nWin + c*nWin + w) = tempB;
-        *(outputY + (s/2+offset)*nChan*nWin + c*nWin + w) = tempA;
-        *(validX + (s/2+offset)*nWin + w) = validB;
-        *(validY + (s/2+offset)*nWin + w) = validA;
-      } else {
-        *(outputX + (s/2+offset)*nChan*nWin + c*nWin + w) = tempA;
-        *(outputY + (s/2+offset)*nChan*nWin + c*nWin + w) = tempB;
-        *(validX + (s/2+offset)*nWin + w) = validA;
-        *(validY + (s/2+offset)*nWin + w) = validB;
+      if( *(swap + s) > 0 ) {
+        tempT = tempA;
+        tempA = tempB;
+        tempB = tempT;
+        
+        validT = validA;
+        validA = validB;
+        validB = validT;
       }
+      
+      *(outputX + (s+offset)*nChan*nWin + c*nWin + w) = tempA;
+      *(outputY + (s+offset)*nChan*nWin + c*nWin + w) = tempB;
+      *(validX + (s+offset)*nWin + w) = validA;
+      *(validY + (s+offset)*nWin + w) = validB;
     }
   }
 }
@@ -105,15 +111,16 @@ void pol_fill(const float2 *signals,
 
 def pol_fill(signals, valid, swap, offset, outputX, validX, outputY, validY, blockDim=(4,16)):
     nStand, nChanAct, nWinAct = signals.shape
-    nChan, nWin = outputX.shape[1], outputX.shape[2]
+    _, nChan, nWin = outputX.shape
+    assert(offset >= 0)
     
     signals = copy_using_cache(signals)
     valid = copy_using_cache(valid)
-    swap =copy_using_cache(swap)
-    outputX = copy_using_cache(outputX)
-    validX = copy_using_cache(validX)
-    outputY = copy_using_cache(outputY)
-    validY = copy_using_cache(validY)
+    swap = copy_using_cache(swap)
+    outputX = copy_using_cache(outputX, tag='pfX')
+    validX = copy_using_cache(validX, tag='pfX')
+    outputY = copy_using_cache(outputY, tag='pfY')
+    validY = copy_using_cache(validY, tag='pfY')
     
     nct, nwt = blockDim
     ncb = int(np.ceil(nChan/nct))
@@ -121,8 +128,8 @@ def pol_fill(signals, valid, swap, offset, outputX, validX, outputY, validY, blo
     
     _POL_FILL((ncb,nwb), (nct,nwt),
               (signals, valid, swap, 
-                cp.int32(nStand), cp.int32(nChanAct), cp.int32(nWinAct),
-                cp.int32(nChan), cp.int32(nWin), cp.int32(offset),
-                outputX, validX, outputY, validY))
+               cp.int32(nStand), cp.int32(nChanAct), cp.int32(nWinAct),
+               cp.int32(nChan), cp.int32(nWin), cp.int32(offset),
+               outputX, validX, outputY, validY))
                 
     return outputX, validX, outputY, validY
