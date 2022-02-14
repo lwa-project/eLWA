75e736a3348edbfa976324359a8434d27cd5903d
jaycedowell <jaycedowell@gmail.com>
2022-02-14 15:14:23 -0700

    Cleanup plus new functions for GPU-only correlation.

--- gpu/xcupy.py
+++ gpu/xcupy.py
@@ -5,19 +5,48 @@ Module that provides GPU-based X-engines built using cupy.
 # Python2 compatibility
 from __future__ import print_function, division
 
-import cupy
-import time
-import numpy
+import cupy as cp
+import numpy as np
 
-__version__ = '0.1'
-__all__ = ['select_gpu', 'get_memory_usage_limit', 'set_memory_usage_limit',
-           'xengine', 'xengine_full']
+from .cache import get_from_shape, get_from_ndarray, copy_using_cache
 
 
-_XENGINE2 = cupy.RawKernel(r"""
-inline __host__ __device__ void operator/=(float2 &a, int b) {
+__version__ = '0.2'
+__all__ = ['xengine', 'combined_xengine_full', 'xengine_full', 'accumulate_vis']
+
+
+_XENGINE = cp.RawModule(code=r"""
+inline __host__ __device__ void operator*=(float2 &a, unsigned char b) {
+  a.x *= (float) b;
+  a.y *= (float) b;
+}
+
+inline __host__ __device__ float2 operator*(float2 a, unsigned char b) {
+  return make_float2(a.x * (float) b, a.y * (float) b);
+}
+
+inline __host__ __device__ void operator/=(float2 &a, float b) {
   a.x /= b;
   a.y /= b;
+}
+
+inline __host__ __device__ float2 operator/(float2 a, float b) {
+  return make_float2(a.x / b, a.y / b);
+}
+
+inline __host__ __device__ float2 conjy(float2 a) {
+  return make_float2(a.x, -a.y);
+}
+
+inline __host__ __device__ float4 pack_pair(float2 a, float2 b) {
+  return make_float4(a.x, a.y, b.x, b.y);
+}
+
+inline __host__ __device__ void unpack_pair(float4 a, float2 &b, float2 &c) {
+  b.x = a.x;
+  b.y = a.y;
+  c.x = a.z;
+  c.y = a.w;
 }
 
 extern "C" __global__
@@ -60,40 +89,6 @@ void xengine2(const float2 *signals1,
     
     *(output + bl*nChan + chan) = tempO;
   }
-}""", 'xengine2')
-
-_XENGINE3 = cupy.RawModule(code=r"""
-inline __host__ __device__ void operator*=(float2 &a, unsigned char b) {
-  a.x *= (float) b;
-  a.y *= (float) b;
-}
-
-inline __host__ __device__ float2 operator*(float2 a, unsigned char b) {
-  return make_float2(a.x * (float) b, a.y * (float) b);
-}
-
-inline __host__ __device__ void operator/=(float2 &a, float b) {
-  a.x /= b;
-  a.y /= b;
-}
-
-inline __host__ __device__ float2 operator/(float2 a, float b) {
-  return make_float2(a.x / b, a.y / b);
-}
-
-inline __host__ __device__ float2 conjy(float2 a) {
-  return make_float2(a.x, -a.y);
-}
-
-inline __host__ __device__ float4 pack_pair(float2 a, float2 b) {
-  return make_float4(a.x, a.y, b.x, b.y);
-}
-
-inline __host__ __device__ void unpack_pair(float4 a, float2 &b, float2 &c) {
-  b.x = a.x;
-  b.y = a.y;
-  c.x = a.z;
-  c.y = a.w;
 }
 
 extern "C" __global__
@@ -211,93 +206,9 @@ void xengine3(const float4 *signals,
 }
 """)
 
-_INTERLEAVE = _XENGINE3.get_function('interleave')
-_XENGINE3 = _XENGINE3.get_function('xengine3')
-
-
-class _MemoryCache(object):
-    def __init__(self, device=None, max_len=10):
-        if device is not None:
-            dev = cupy.cuda.Device(device)
-        else:
-            dev = cupy.cuda.Device()
-            device = dev.id
-        dev.use()
-        self._device = device
-        
-        self._pool = cupy.get_default_memory_pool()
-        self._cache = {}
-        self._created = {}
-        self._max_len = int(max_len)
-        
-    def __getitem__(self, key):
-        try:
-            value = self._cache[key]
-        except KeyError:
-            try:
-                value = cupy.empty(shape=tuple([k for k in key[:-1]]), dtype=key[-1])
-            except cupy.cuda.memory.OutOfMemoryError:
-                self.free()
-                value = cupy.empty(shape=tuple([k for k in key[:-1]]), dtype=key[-1])
-            self.__setitem__(key, value)
-        self._created[key] = time.time()
-        return value
-        
-    def __setitem__(self, key, value):
-        if len(self._cache) > self._max_len:
-            oldest = 0
-            oldest_key = None
-            for k in self._cache:
-                age = time.time() - self._created[k]
-                if age > oldest:
-                    oldest = age
-                    oldest_key = k
-            del self._cache[oldest_key]
-            del self._created[oldest_key]
-            self.free()
-            
-        self._cache[key] = value
-        self._created[key] = time.time()
-        
-    def get_limit(self):
-        return self._pool.get_limit()
-        
-    def set_limit(self, size_bytes):
-        self._pool.set_limit(size=int(size_bytes))
-        
-    def free(self):
-        self._pool.free_all_blocks()
-        
-    def select_gpu(self, device=0):
-        if self._device == device:
-            return
-            
-        size_bytes = self.get_limit()
-        self._cache.clear()
-        self._created.clear()
-        self.free()
-        
-        cupy.cuda.Device(device).use()
-        self._pool = cupy.get_default_memory_pool()
-        self._cache = {}
-        self._created = {}
-        self._device = device
-        self.set_limit(size_bytes)
-
-
-_CACHE = _MemoryCache()
-
-
-def select_gpu(device=0):
-    _CACHE.select_gpu(device)
-
-
-def get_memory_usage_limit():
-    return _CACHE.get_limit()
-
-
-def set_memory_usage_limit(size_bytes):
-    _CACHE.set_limit(size_bytes)
+_XENGINE2 = _XENGINE.get_function('xengine2')
+_INTERLEAVE = _XENGINE.get_function('interleave')
+_XENGINE3 = _XENGINE.get_function('xengine3')
 
 
 def xengine(signalsF1, validF1, signalsF2, validF2, blockDim=(4,16)):
@@ -308,36 +219,63 @@ def xengine(signalsF1, validF1, signalsF2, validF2, blockDim=(4,16)):
     nStand, nChan, nWin = signalsF1.shape
     nBL = nStand*(nStand+1) // 2
     
-    with cupy.cuda.Stream():
-        try:
-            signalsF1 = cupy.asarray(signalsF1)
-            signalsF2 = cupy.asarray(signalsF2)
-            validF1 = cupy.asarray(validF1)
-            validF2 = cupy.asarray(validF2)
-        except cupy.cuda.memory.OutOfMemoryError:
-            _CACHE.free()
-            signalsF1 = cupy.asarray(signalsF1)
-            signalsF2 = cupy.asarray(signalsF2)
-            validF1 = cupy.asarray(validF1)
-            validF2 = cupy.asarray(validF2)
-            
-        try:
-            output = _CACHE[(1,nBL,nChan,numpy.complex64)]
-        except KeyError:
-            output = cupy.empty((nBL,nChan), dtype=numpy.complex64)
-            _CACHE[(1,nBL,nChan,numpy.complex64)] = output
-            
-        nbt, nct = blockDim
-        nbb = int(numpy.ceil(nBL/nbt))
-        ncb = int(numpy.ceil(nChan/nct))
+    signalsF1 = cp.asarray(signalsF1)
+    signalsF2 = cp.asarray(signalsF2)
+    validF1 = cp.asarray(validF1)
+    validF2 = cp.asarray(validF2)
+    
+    output = get_from_shape((1,nBL,nChan), dtype=np.complex64)
+    
+    nbt, nct = blockDim
+    nbb = int(np.ceil(nBL/nbt))
+    ncb = int(np.ceil(nChan/nct))
+    
+    _XENGINE2((nbb,ncb), (nbt, nct),
+              (signalsF1, signalsF2, validF1, validF2,
+               cp.int32(nStand), cp.int32(nBL), cp.int32(nChan), cp.int32(nWin),
+               output))
+    
+    return output
+
+
+def combined_xengine_full(signalsFX, validFX, signalsFY, validFY, blockDim=(4,16)):
+    """
+    X-engine for the outputs of fengine() where all four polarization products
+    are in a single array.
+    """
+    
+    nStand, nChan, nWin = signalsFX.shape
+    nBL = nStand*(nStand+1) // 2
+    
+    signalsFX = copy_using_cache(signalsFX)
+    signalsFY = copy_using_cache(signalsFY)
+    validFX = copy_using_cache(validFX)
+    validFY = copy_using_cache(validFY)
+    
+    combined = get_from_shape((2*nStand,nChan,nWin), dtype=np.complex64)
+    valid = get_from_shape((2*nStand,nWin), dtype=np.uint8)
+    
+    nct, nwt = blockDim
+    ncb = int(np.ceil(nChan/nct))
+    nwb = int(np.ceil(nWin/nwt))
         
-        _XENGINE2((nbb,ncb), (nbt, nct),
-                  (signalsF1, signalsF2, validF1, validF2,
-                   cupy.int32(nStand), cupy.int32(nBL), cupy.int32(nChan), cupy.int32(nWin),
-                   output))
-        
-        output_cpu = cupy.asnumpy(output)
-    return output_cpu
+    _INTERLEAVE((nStand,ncb,nwb), (nct,nwt),
+                (signalsFX, signalsFY, validFX, validFY,
+                 cp.int32(nStand), cp.int32(nChan), cp.int32(nWin),
+                 combined, valid))
+    
+    output = get_from_shape((4,nBL,nChan), dtype=np.complex64, tag='fvis')
+    
+    nbt, nct = blockDim
+    nbb = int(np.ceil(nBL/nbt))
+    ncb = int(np.ceil(nChan/nct))
+    
+    _XENGINE3((nbb,ncb), (nbt, nct),
+              (combined, valid,
+               cp.int32(nStand), cp.int32(nBL), cp.int32(nChan), cp.int32(nWin),
+                output))
+    
+    return output
 
 
 def xengine_full(signalsFX, validFX, signalsFY, validFY, blockDim=(4,16)):
@@ -345,54 +283,58 @@ def xengine_full(signalsFX, validFX, signalsFY, validFY, blockDim=(4,16)):
     X-engine for the outputs of fengine().
     """
     
-    nStand, nChan, nWin = signalsFX.shape
-    nBL = nStand*(nStand+1) // 2
+    output = combined_xengine_full(signalsFX, validFX, signalsFY, validFY, blockDim=blockDim)
+    return output[0,:,:], output[1,:,:], output[2,:,:], output[3,:,:]
+
+
+_ACCUMULATE = cp.RawKernel(r"""
+inline __host__ __device__ void operator*=(float2 &a, float b) {
+  a.x *= b;
+  a.y *= b;
+}
+
+inline __host__ __device__ void operator+=(float2 &a, float2 b) {
+  a.x += b.x;
+  a.y += b.y;
+}
+
+extern "C" __global__
+void accumulate(const float2 *input,
+                int nPol,
+                int nBL,
+                int nChan,
+                int reset,
+                float scale,
+                float2 *output) {
+  int p = blockIdx.x;
+  int b = blockIdx.y*blockDim.x + threadIdx.x;
+  int c = blockIdx.z*blockDim.y + threadIdx.y;
+  
+  if( b >= nBL || c >= nChan ) {
+    // Nothin'
+  } else {
+    float2 temp = *(input +p*nBL*nChan + b*nChan + c);
+    temp *= scale;
+    if( reset > 0 ) {
+      *(output + p*nBL*nChan + b*nChan + c) = make_float2(0, 0);
+    }
+    *(output + p*nBL*nChan + b*nChan + c) += temp;
+  }
+}
+""", 'accumulate')
+
+def accumulate_vis(vis_accum, vis, scale=1, reset=False, blockDim=(4,16)):
+    nPol, nBL, nChan = vis.shape
     
-    with cupy.cuda.Stream():
-        try:
-            signalsFX = cupy.asarray(signalsFX)
-            signalsFY = cupy.asarray(signalsFY)
-            validFX = cupy.asarray(validFX)
-            validFY = cupy.asarray(validFY)
-        except cupy.cuda.memory.OutOfMemoryError:
-            _CACHE.free()
-            signalsFX = cupy.asarray(signalsFX)
-            signalsFY = cupy.asarray(signalsFY)
-            validFX = cupy.asarray(validFX)
-            validFY = cupy.asarray(validFY)
-            
-        try:
-            combined = _CACHE[(2*nStand,nChan,nWin,numpy.complex64)]
-            valid = _CACHE[(2*nStand,nWin,numpy.uint8)]
-        except KeyError:
-            combined = cupy.empty((2*nStand,nChan,nWin), dtype=numpy.complex64)
-            valid = cupy.empty((2*nStand,nWin), dtype=numpy.uint8)
-            _CACHE[(2*nStand,nChan,nWin,numpy.complex64)] = combined
-            _CACHE[(2*nStand,nWin,numpy.uint8)] = valid
-            
-        nct, nwt = blockDim
-        ncb = int(numpy.ceil(nChan/nct))
-        nwb = int(numpy.ceil(nWin/nwt))
-            
-        _INTERLEAVE((nStand,ncb,nwb), (nct,nwt),
-                    (signalsFX, signalsFY, validFX, validFY,
-                     cupy.int32(nStand), cupy.int32(nChan), cupy.int32(nWin),
-                     combined, valid))
-        
-        try:
-            output = _CACHE[(4,nBL,nChan,numpy.complex64)]
-        except KeyError:
-            output = cupy.empty((4,nBL,nChan), dtype=numpy.complex64)
-            _CACHE[(4,nBL,nChan,numpy.complex64)] = output
-            
-        nbt, nct = blockDim
-        nbb = int(numpy.ceil(nBL/nbt))
-        ncb = int(numpy.ceil(nChan/nct))
-        
-        _XENGINE3((nbb,ncb), (nbt, nct),
-                  (combined, valid,
-                   cupy.int32(nStand), cupy.int32(nBL), cupy.int32(nChan), cupy.int32(nWin),
-                    output))
-        
-        output_cpu = cupy.asnumpy(output)
-    return output_cpu[0,:,:], output_cpu[1,:,:], output_cpu[2,:,:], output_cpu[3,:,:]
+    vis_accum = copy_using_cache(vis_accum)
+    vis = copy_using_cache(vis)
+    
+    nbt, nct = blockDim
+    nbb = int(np.ceil(nBL/nbt))
+    ncb = int(np.ceil(nChan/nct))
+    npb = nPol
+    
+    _ACCUMULATE((npb, nbb,ncb), (nbt, nct),
+                (vis, cp.int32(nPol), cp.int32(nBL), cp.int32(nChan),
+                 cp.int32(reset), cp.float32(scale), vis_accum))
+    return vis_accum
